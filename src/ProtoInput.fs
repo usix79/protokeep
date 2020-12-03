b9d96961a700e643fc8ff92451b31e3776b7402a
usix <serhii.sabirov@gmail.com>
2020-12-03 15:05:37 +0200

    parsing of the input format

--- src/ProtoInput.fs
+++ src/ProtoInput.fs
@@ -2,13 +2,48 @@ module Protogen.Input
 
 open FParsec
 
-type ModuleName = string list
+type ComplexName = ComplexName of string list
+
+type Type =
+    | Bool
+    | String
+    | Int
+    | Long
+    | Decimal of scale: int
+    | Float
+    | Double
+    | Bytes
+    | Timespamp
+    | Duration
+    | Guid
+    | Optional of value: Type
+    | Array of value: Type
+    | Map of value: Type
+    | Complex of name: ComplexName
+
+type EnumInfo = { Name: string; Symbols: string list }
+type FieldInfo = { Name: string;  Type: Type }
+type RecordInfo = { Name: string;  Fields: FieldInfo list }
+type UnionCaseFieldInfo = { Name: string option;  Type: Type }
+type UnionCaseInfo = {Name: string; Fields: UnionCaseFieldInfo list}
+type UnionInfo = {Name: string;  Cases: UnionCaseInfo list}
+
+type ModuleItem =
+    | Enum of EnumInfo
+    | Record of RecordInfo
+    | Union of UnionInfo
 
 type Module = {
-    Name: ModuleName
+    Name: ComplexName
+    Items: ModuleItem list
 }
 
 module private Parser =
+
+    let ws<'u> : Parser<_,'u> = skipMany (skipAnyOf " \t")
+    let ws1<'u> : Parser<_,'u> = skipMany1 (skipAnyOf " \t")
+    let ts<'u> : Parser<_,'u> = ws >>. (skipNewline <|> eof)  // trailing spaces
+    let keyword name = pstring name >>. ws1
 
     let identifier =
         let isIdentifierFirstChar c = isLetter c || c = '_'
@@ -16,16 +51,82 @@ module private Parser =
 
         many1Satisfy2L isIdentifierFirstChar isIdentifierChar "identifier"
 
-    let moduleName : Parser<ModuleName, unit> =
-        sepBy1 identifier (pchar '.')
+    let fullName : Parser<ComplexName, _> =
+        sepBy1 identifier (pchar '.') |>> ComplexName
+
+    let enum' =
+        keyword "enum" >>.
+            pipe2
+                (identifier .>> ws .>> skipChar '=' .>> spaces .>> opt (pchar '|'))
+                (sepBy1 (between spaces spaces identifier) (pchar '|') )
+                (fun name symbols -> Enum {Name = name; Symbols = symbols})
+
+    let type' =
+        choice[
+            skipString "bool" |>> (fun () -> Bool)
+            skipString "string" |>> (fun () -> String)
+            skipString "int" |>> (fun () -> Int)
+            skipString "long" |>> (fun () -> Long)
+            skipString "float" |>> (fun () -> Float)
+            skipString "double" |>> (fun () -> Double)
+            skipString "bytes" |>> (fun () -> Bytes)
+            skipString "timestamp" |>> (fun () -> Timespamp)
+            skipString "duration" |>> (fun () -> Duration)
+            skipString "guid" |>> (fun () -> Guid)
+            skipString "decimal" >>. ws >>. pchar '(' >>. ws >>. pint32 .>> ws .>> pchar ')' |>> Decimal
+            fullName |>> Complex]
+
+    let fullType' =
+        type' >>= (fun t ->
+            ws >>.
+            opt (choice[
+                    skipString "option" |>> (fun () -> Optional t)
+                    skipString "array" |>> (fun () -> Array t)
+                    skipString "map" |>> (fun () -> Map t)])
+            |>> (Option.defaultValue t))
+
+    let field' =
+        pipe2
+            ((between spaces ws identifier) .>> pchar ':')
+            (between ws ws fullType')
+            (fun name type' -> {Name = name; Type = type'} : FieldInfo)
+
+    let record' =
+            keyword "record" >>.
+                pipe2
+                    (identifier .>> ws .>> skipChar '=' .>> spaces)
+                    (between (pchar '{') (pchar '}' .>> spaces)
+                        (sepEndBy field' (pchar ';' <|> newline)))
+                    (fun name fields -> Record {Name = name; Fields = fields})
+
+    let unionCaseField' =
+        pipe2
+            (opt ((between ws ws identifier) .>>? pchar ':'))
+            (between ws ws fullType')
+            (fun name type' -> {Name = name; Type = type'} : UnionCaseFieldInfo)
+
+    let unionCase' =
+        pipe2
+            identifier
+            (opt (ws1 .>>? (keyword "of") >>. sepBy unionCaseField' (pchar '*')))
+            (fun name fields -> {Name = name; Fields = fields |> Option.defaultValue []} : UnionCaseInfo)
+
+    let union' =
+        keyword "union" >>.
+            pipe2
+                (identifier .>> ws .>> skipChar '=' .>> spaces .>> opt (pchar '|'))
+                (sepBy1 (between spaces spaces unionCase') (pchar '|') )
+                (fun name cases -> Union {Name = name; Cases = cases})
 
     let module' =
-        pstring "module" >>. spaces >>. moduleName
-        |>> (fun name -> {Name = name})
-        .>> spaces
+        keyword "module" >>.
+            pipe2
+                (fullName .>> ts)
+                (many (choice [enum'; record'; union']))
+                (fun name items -> {Name = name; Items = items})
 
     let file =
-        spaces >>. many module' .>> eof
+        spaces >>. many (module' .>> spaces) .>> eof
 
 let parse input =
     match run Parser.file input with
--- tests/ProtoInputTests.fs
+++ tests/ProtoInputTests.fs
@@ -11,23 +11,194 @@ let private assertParse input expected =
 
 [<Fact>]
 let ``Test empty input`` () =
-    let input = ""
-    assertParse input []
+    assertParse "" []
 
 [<Fact>]
-let ``Test single module`` () =
+let ``Test single empty module`` () =
     let input = "module Domain.Foundation"
-    assertParse input [{Name = ["Domain"; "Foundation"]}]
+    assertParse input [{Name = ComplexName ["Domain"; "Foundation"]; Items = []}]
 
 
 [<Fact>]
-let ``Test many modules`` () =
+let ``Test many empty modules`` () =
     let input = """
 module Domain
 module Domain.Foundation
 """
     let expected = [
-            {Name = ["Domain"]}
-            {Name = ["Domain"; "Foundation"]}]
+            {Name = ComplexName ["Domain"]; Items = []}
+            {Name = ComplexName ["Domain"; "Foundation"]; Items = []}]
 
     assertParse input expected
+
+[<Fact>]
+let ``Test enum`` () =
+    let input = """
+module Domain
+enum TrafficLight =
+    | Red
+    | Yellow
+    | Green
+"""
+    assertParse input [{Name = ComplexName ["Domain"]; Items = [Enum {Name = "TrafficLight"; Symbols = ["Red"; "Yellow"; "Green"]}]}]
+
+[<Fact>]
+let ``Test single line enum`` () =
+    let input = """
+module Domain
+enum TrafficLight = Red | Yellow | Green
+"""
+    assertParse input [{Name = ComplexName ["Domain"]; Items = [Enum {Name = "TrafficLight"; Symbols = ["Red"; "Yellow"; "Green"]}]}]
+
+[<Fact>]
+let ``Test record`` () =
+    let input = """
+module Domain
+record Crossroad = {
+    Id: int
+    LongId: long
+    AltId: guid
+    Street1: string
+    Street2: string
+    IsMonitored: bool
+    Xpos: float
+    Ypos: double
+    Ratio: decimal(2)
+    LastChecked: timestamp
+    ServiceInterval: duration
+    CurrentLight: Domain.TrafficLight
+    Nickname: string option
+    Img: bytes
+    Notes: string array
+    Props: string map
+}
+"""
+    assertParse input [{
+        Name = ComplexName ["Domain"]
+        Items = [
+            Record {
+                Name = "Crossroad";
+                Fields = [
+                    { Name = "Id"; Type = Int }
+                    { Name = "LongId"; Type = Long }
+                    { Name = "AltId"; Type = Guid }
+                    { Name = "Street1"; Type = String }
+                    { Name = "Street2"; Type = String }
+                    { Name = "IsMonitored"; Type = Bool }
+                    { Name = "Xpos"; Type = Float }
+                    { Name = "Ypos"; Type = Double }
+                    { Name = "Ratio"; Type = Decimal 2 }
+                    { Name = "LastChecked"; Type = Timespamp }
+                    { Name = "ServiceInterval"; Type = Duration }
+                    { Name = "CurrentLight"; Type = Complex (ComplexName ["Domain"; "TrafficLight"]) }
+                    { Name = "Nickname"; Type = Optional String }
+                    { Name = "Img"; Type = Bytes }
+                    { Name = "Notes"; Type = Array String }
+                    { Name = "Props"; Type = Map String }
+                ]}]}]
+
+
+[<Fact>]
+let ``Test single line record`` () =
+    let input = """
+module Domain
+record Crossroad = { Id: int; Street1: string; Street2: string }
+"""
+    assertParse input [{
+        Name = ComplexName ["Domain"]
+        Items = [
+            Record {
+                Name = "Crossroad";
+                Fields = [
+                    { Name = "Id"; Type = Int }
+                    { Name = "Street1"; Type = String }
+                    { Name = "Street2"; Type = String }
+                ]}]}]
+
+
+[<Fact>]
+let ``Test union`` () =
+    let input = """
+module Domain
+union ServiceCheck =
+    | Random
+    | Planned of timestamp
+    | Campaign of name:string*step:int
+    | RCA of Incident
+"""
+    assertParse input [{
+        Name = ComplexName ["Domain"]
+        Items = [
+            Union {
+                Name = "ServiceCheck";
+                Cases = [
+                    { Name = "Random"; Fields = []}
+                    { Name = "Planned"; Fields = [{Name = None; Type = Timespamp}] }
+                    { Name = "Campaign"; Fields = [
+                        {Name = Some "name"; Type = String}
+                        {Name = Some "step"; Type = Int}
+                        ]}
+                    { Name = "RCA"; Fields = [{Name = None; Type = Complex (ComplexName ["Incident"])}] }
+                ]}]}]
+
+[<Fact>]
+let ``Test single line union`` () =
+    let input = """
+module Domain
+union ServiceCheck = Random | Planned of timestamp | Campaign of name:string*step:int | RCA of Incident
+"""
+    assertParse input [{
+        Name = ComplexName ["Domain"]
+        Items = [
+            Union {
+                Name = "ServiceCheck";
+                Cases = [
+                    { Name = "Random"; Fields = []}
+                    { Name = "Planned"; Fields = [{Name = None; Type = Timespamp}] }
+                    { Name = "Campaign"; Fields = [
+                        {Name = Some "name"; Type = String}
+                        {Name = Some "step"; Type = Int}
+                        ]}
+                    { Name = "RCA"; Fields = [{Name = None; Type = Complex (ComplexName ["Incident"])}] }
+                ]}]}]
+
+
+[<Fact>]
+let ``Complex Test`` () =
+    let input = """
+module Domain
+enum TrafficLight = Red | Yellow | Green
+
+module Domain.Foundation
+enum AltTrafficLight = Red | Yellow | Blue
+record Crossroad = { Id: int; Street1: string; Street2: string }
+union ServiceCheck = Random | Planned of timestamp | Campaign of name:string*step:int | RCA of Incident
+"""
+    assertParse input [
+        {   Name = ComplexName ["Domain"]
+            Items = [Enum {Name = "TrafficLight"; Symbols = ["Red"; "Yellow"; "Green"]}] }
+        {   Name = ComplexName ["Domain"; "Foundation"]
+            Items = [
+                Enum {
+                    Name = "AltTrafficLight";
+                    Symbols = ["Red"; "Yellow"; "Blue"] }
+                Record { Name = "Crossroad";
+                    Fields =
+                    [
+                        { Name = "Id"; Type = Int }
+                        { Name = "Street1"; Type = String }
+                        { Name = "Street2"; Type = String }
+                    ]}
+                Union {
+                    Name = "ServiceCheck";
+                    Cases = [
+                        { Name = "Random"; Fields = []}
+                        { Name = "Planned"; Fields = [{ Name = None; Type = Timespamp }]}
+                        { Name = "Campaign"; Fields = [
+                            {Name = Some "name"; Type = String}
+                            {Name = Some "step"; Type = Int}
+                            ]}
+                        { Name = "RCA"; Fields = [{Name = None; Type = Complex (ComplexName ["Incident"])}] }
+                    ]}
+                ]}
+    ]
